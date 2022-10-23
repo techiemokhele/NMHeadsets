cabce8061ed8db18a0ccdc2b92b57d1d3e2c08ac
techiemokhele <neomokhele23@gmail.com>
2022-10-23 10:22:20 +0200

    project commit

---
+++ screens/MyCartScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MyCartScreen = () => {
+  return (
+    <View>
+      <Text>MyCartScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MyCartScreen;
---
+++ screens/ProductInfoScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProductInfoScreen = () => {
+  return (
+    <View>
+      <Text>ProductInfoScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductInfoScreen;
